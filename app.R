99ed812185e39c2f3871429834a1f67cae179ca1
abitter <42348155+abitter@users.noreply.github.com>
2018-12-04 20:17:22 +0100

    Update app.R

--- app.R
+++ app.R
@@ -15,8 +15,7 @@ library(lattice)
 
 # data ----
 theta_year <- readRDS("data/theta_year.rds")
-theta_mean_by_year <- readRDS("data/theta_year.rds")
-colnames(theta_mean_by_year) <- 1:500
+theta_mean_by_year <- readRDS("data/theta_mean_by_year.rds")
 theta_mean_by_year_time <- readRDS("data/theta_time.rds")
 theta_mean_by_year_ts <- readRDS("data/theta_ts.rds")
 years <- readRDS("data/years.rds")
