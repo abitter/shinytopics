dfcffd936cc85c6787c8bbf7b2be8eef8d997a18
abitter <42348155+abitter@users.noreply.github.com>
2018-12-08 08:49:27 +0100

    Update trends.R

--- trends.R
+++ trends.R
@@ -1,6 +1,7 @@
 # Trends for range input
 ########################
 
+# original code: https://github.com/mponweiser/LDA-snippets/blob/master/stefano-prova.r
 
 #von <- input$range[1]-1979
 #bis <- input$range[2]-1979
