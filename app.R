afa8b3aeaf886ecff30865178674cebe2f553d71
André Bittermann <42348155+abitter@users.noreply.github.com>
2018-12-17 19:30:25 +0100

    v.0.4.2

--- app.R
+++ app.R
@@ -266,15 +266,16 @@ server <- function(input, output, session) {
   
   output$circleplot <- renderPlot({
     plot(1, xlab="", ylab="", xaxt='n', yaxt='n', asp = 1, xlim = c(0.6, 1.4), ylim = c(0.6, 1.4),
-         main = list(paste0("Prävalenz von Thema ", select(), ": ", round(topic[select(),3], 4)), par(cex.main = 1)), type="n")
+         #main = list(paste0("Prävalenz von Thema ", select(), ": ", round(topic[select(),3], 4)), par(cex.main = 1)), type="n")
+         main = list(paste0("Prävalenz von Thema ", select(), " im Vergleich "), par(cex.main = 1)), type="n")
     plotrix::draw.circle(1, 1, topic[select(),3]*10, col="gold", border="gold") # current topic
     plotrix::draw.circle(1, 1, (1/(dim(topic)[1]))*10, border="steelblue3", col="white", lty="solid", density=0) # average
-    plotrix::draw.circle(1, 1, max(topic[,3])*10, border="black", col="white", lty="dashed", density=0) # max
+    plotrix::draw.circle(1, 1, max(topic[,3])*10, border="black", col="white", lty="dotted", density=0) # max
     #plotrix::draw.circle(1, 1, min(topic[,3])*10, border="black", col="white", lty="solid", density=0) # min
     legend("bottomright",
            legend=c("Maximum", "Durchschnitt"), 
            col=c("black", "steelblue3"), 
-           lty=c("dashed", "solid"), 
+           lty=c("dotted", "solid"), 
            cex=0.6)
     
   }, res=125)
